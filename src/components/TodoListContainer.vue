2494f030f4ec3a907afffcbf1a16b6100a2c72fc
Dong-min Kwak <kdm0324@naver.com>
2023-04-13 00:17:05 +0900

    first commit

---
+++ src/components/TodoListContainer.vue
@@ -0,0 +1,25 @@
+<template></template>
+
+<script>
+import { ref, readonly, provide } from 'vue'
+export default {
+    name: 'TodoListContainer',
+    setup() {
+        const todos = ref([]);
+        provide('todos', readonly(todos));
+        const initTodos = (init_todos) => {
+            todos.value = init_todos
+        }
+
+        const addTodo = (todo, date) => {}
+        const removeTodo = (id) => {
+
+        }
+        const completeTodo = (id) => {}
+
+        provide('addTodo', addTodo);
+        provide('removeTodo', removeTodo);
+        provide('completeTodo', completeTodo);
+    },
+}
+</script>
